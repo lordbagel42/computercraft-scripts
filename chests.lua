585abc1556a509de70447e9ddd2b444bd0e99ed4
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-03 18:28:38 -0600

    idk anymore

--- chests.lua
+++ chests.lua
@@ -1,4 +1,8 @@
--- Function to parse command-line arguments
+local x, y, z = gps.locate()
+
+-- north = 0, east = 1, south = 2, west = 3
+local currentDirection = 2 -- south
+
 local function parseArgs()
     if #arg < 3 then
         print("Usage: goTo <x> <y> <z>")
@@ -17,45 +21,16 @@ local function parseArgs()
     return targetX, targetY, targetZ
 end
 
--- Function to get the current position using GPS
-local function getCurrentPosition()
-    local x, y, z = gps.locate()
-    if x == nil or y == nil or z == nil then
-        print("Unable to determine current position. Make sure you're in range of GPS.")
-        return nil
-    end
-    return x, y, z
-end
+local function turnTo(targetHeading) -- takes in a number
+	while targetHeading < currentDirection then
+		turtle.turnLeft()
+		currentDirection = currentDirection - 1
+		if currentDirection == targetHeading then
+			currentDirection = targetHeading
+		end
+	end
 
--- Function to turn the turtle to face a specific direction
-local function turnTo(direction)
-    local currentDirection = "south"  -- Assuming the turtle starts facing south
-    local directions = {"north", "east", "south", "west"}
-    local dirMap = {north=0, east=1, south=2, west=3}
-    
-    local function turnLeft()
-        turtle.turnLeft()
-        currentDirection = directions[(dirMap[currentDirection] - 1) % 4 + 1]
-    end
-    
-    local function turnRight()
-        turtle.turnRight()
-        currentDirection = directions[(dirMap[currentDirection] + 1) % 4 + 1]
-    end
-    
-    while currentDirection ~= direction do
-        turnRight()
-    end
-end
-
--- Function to move the turtle to the specified coordinates
 local function moveTo(targetX, targetY, targetZ)
-    local currentX, currentY, currentZ = getCurrentPosition()
-    if currentX == nil then
-        return
-    end
+end
 
-    -- Move in the X direction
-    while currentX ~= targetX do
-        if currentX < targetX then
-            turnTo("east
+local targetX, targetY, targetZ = parseArgs()
